160d5e5addb9672e85e1bf98784e9998d6763568
Henrique Santos <141459753+Rick1052@users.noreply.github.com>
2026-03-16 17:24:59 -0300

    ajuste porta

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config()
+
 import app from './app.js'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
 
 server.on('error', (err) => {
-  console.error("Failed to start server:", err.message);
-  process.exit(1);
-});
+  console.error("Failed to start server:", err.message)
+  process.exit(1)
+})
